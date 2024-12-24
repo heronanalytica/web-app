ff0f33599a4bc7f54eae6d681b33888b5ade4ca2
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2024-12-23 21:57:23 -0800

    Update form config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,10 +49,33 @@ export default function Home() {
         <div>
           {/* @ts-ignore */}
           <form name="contact" method="POST" netlify>
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message"></textarea>
-            <button type="submit">Send</button>
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{" "}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
           </form>
         </div>
       </main>
