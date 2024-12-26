31a13fd99db39b31342a2c516fdb803e955010a1
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2024-12-26 00:08:48 -0800

    Add GA setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-import ClientLayout from "./clientLayout"; // Import the client component
+import ClientLayout from "./clientLayout";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -19,8 +20,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
+
   return (
     <html lang="en">
+      <head>
+        {APP_ENV === "production" && (
+          <>
+            {/* Google Analytics Scripts */}
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+              `}
+            </Script>
+          </>
+        )}
+      </head>
       <body className={`${roboto.variable}`}>
         {/* Render the client-side layout */}
         <ClientLayout>{children}</ClientLayout>
