5c4827a548433eed009fadd7f8681a8d529f37f2
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2025-07-15 22:20:15 -0700

    [FE] Add discard button

--- web/src/app/app/campaign/new/page.tsx
+++ web/src/app/app/campaign/new/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { useState } from "react";
-import styles from "../../styles.module.scss";
+import { Button, Modal } from "antd";
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
 import Steps from "../../components/Steps";
-
 import CustomerFileStep from "../components/CustomerFileStep";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/constants/routes";
 
 const NewCampaignPage = () => {
   const [currentStep, setCurrentStep] = useState<number>(0);
-  const [, setSelectedCustomerFile] = useState<
-    string | null
-  >(null);
+  const router = useRouter();
 
-  const handleFileSelected = (fileId: string) => {
-    setSelectedCustomerFile(fileId);
-    setCurrentStep(1); // Advance to next step
+  const handleFileSelected = () => {
+    setCurrentStep(1);
   };
 
   const renderStepComponent = () => {
@@ -28,11 +27,41 @@ const NewCampaignPage = () => {
     }
   };
 
+  const [discardModalVisible, setDiscardModalVisible] = React.useState(false);
+
+  const handleDiscard = () => setDiscardModalVisible(true);
+  const handleDiscardConfirm = () => {
+    setDiscardModalVisible(false);
+    router.push(ROUTES.APP_HOMEPAGE);
+  };
+  const handleDiscardCancel = () => setDiscardModalVisible(false);
+
   return (
-    <div className={styles.appContainer}>
-      <Steps totalSteps={5} active={currentStep} />
-      <div className={styles.componentContainer}>{renderStepComponent()}</div>
-    </div>
+    <>
+      <div className={styles.appContainer}>
+        <Steps totalSteps={5} active={currentStep} />
+        <div className={styles.componentContainer}>{renderStepComponent()}</div>
+        <div className={styles.appFooter}>
+          <Button danger onClick={handleDiscard}>
+            Discard
+          </Button>
+        </div>
+      </div>
+      <Modal
+        open={discardModalVisible}
+        title="Discard changes?"
+        onOk={handleDiscardConfirm}
+        onCancel={handleDiscardCancel}
+        okText="Discard"
+        cancelText="Cancel"
+        okButtonProps={{ type: "primary", danger: true }}
+      >
+        <p>
+          Are you sure you want to discard your changes? This action cannot be
+          undone.
+        </p>
+      </Modal>
+    </>
   );
 };
 
